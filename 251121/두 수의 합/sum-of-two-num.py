f6e26523732d8087ec3f790bc5e592ff3d9fa5a8
han sun woo <120350053+han000woo@users.noreply.github.com>
2025-11-21 22:35:26 +0900

    Title: 두 수의 합, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251121/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,18 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+visited_dict = {} 
+for i in arr :
+    visited_dict[i] = False 
+
+count = 0
+for key in visited_dict.keys() :
+    if(visited_dict[key]) :
+        continue 
+    alter_key = k - key 
+    if(alter_key in visited_dict) :
+        visited_dict[alter_key] = True 
+        count +=1 
+
+print(count)
