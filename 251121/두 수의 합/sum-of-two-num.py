c416a87d3485d6952e4d6859c1d4d4b6c09b1c55
han sun woo <120350053+han000woo@users.noreply.github.com>
2025-11-21 22:49:46 +0900

    Title: 두 수의 합, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251121/두 수의 합/sum-of-two-num.py
+++ 251121/두 수의 합/sum-of-two-num.py
@@ -1,27 +1,35 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-visited_dict = {} 
-for i in arr :
-    if i in visited_dict :
-        visited_dict[i] +=1 
-    else : 
-        visited_dict[i] = 1
+visited = {}
+
+for x in arr:
+    visited[x] = visited.get(x, 0) + 1
 
 count = 0
-for key in visited_dict.keys() :
-    if(visited_dict[key] == 0) :
-        continue
-    alter_key = k - key 
-    if(alter_key not in visited_dict) :
-        continue
-    if(visited_dict[alter_key] == 0) :
-        continue 
-    visited_dict[key] -= 1
-    visited_dict[alter_key] -=1
-    count +=1
 
-        
+for key in list(visited.keys()):
+    alter = k - key
+
+    # 이미 사용됨
+    if visited.get(key, 0) == 0:
+        continue
+    
+    # 상대값이 없음
+    if alter not in visited:
+        continue
+
+    # key == alter_key (같은 수 두 개로 쌍 만들기)
+    if key == alter:
+        count += visited[key] // 2
+        visited[key] = 0
+        continue
+
+    # key < alter 일 때만 처리 (중복 방지)
+    if key < alter:
+        pairs = min(visited[key], visited[alter])
+        count += pairs
+        visited[key] -= pairs
+        visited[alter] -= pairs
 
 print(count)
