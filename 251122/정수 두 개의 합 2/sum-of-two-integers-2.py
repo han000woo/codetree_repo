a468e261c8425ffbba9c1f116e141d8b4027fb69
han sun woo <120350053+han000woo@users.noreply.github.com>
2025-11-22 19:07:00 +0900

    Title: 정수 두 개의 합 2, Time: 110ms, Memory: 18MB, Status: Passed - Codetree

--- 251122/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 251122/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -2,21 +2,16 @@ n, k = map(int, input().split())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-arr.sort() 
-
+arr.sort()
 left = 0
+right = n - 1
 count = 0
-right = 1
-len_Arr = len(arr)
-while(left < right) : 
-    # print(left,right)
-    while( arr[left] + arr[right] <= k) :
-        # print("inner",left,right)
-        count += 1
-        if(right + 1 == len_Arr) :
-            break
-        right +=1
 
-    left += 1
+while left < right:
+    if arr[left] + arr[right] <= k:
+        count += (right - left)
+        left += 1
+    else:
+        right -= 1
+
 print(count)
-
