05c61932c35f59cf88c2e890cbc4ed01aeeac0e0
han sun woo <120350053+han000woo@users.noreply.github.com>
2025-11-22 18:51:16 +0900

    Title: 정수 두 개의 합 2, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251122/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 251122/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -5,22 +5,11 @@ arr = [int(input()) for _ in range(n)]
 arr.sort() 
 
 left = 0
+count = 0
+for right in range(1, len(arr)) :
 
-right = len(arr)-1
-count = 0 
-
-while(left <right) :
-    # print(left,right)
-    sum_v = arr[left] + arr[right]
-    if(sum_v < k) :
+    while(arr[left] + arr[right] <= k) :
+        count += 1
         left +=1 
-        right +=1
-        count +=1 
-    elif(sum_v == k) :
-        right -=1 
-        count +=1 
-    elif(sum_v > k ) :
-        right -=1 
-
 print(count)
 
